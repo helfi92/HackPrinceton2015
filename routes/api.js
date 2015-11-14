cebe77861ebeb61c283bde60540d7d09a750dad8
Hassan Ali <helfi92@gmail.com>
2015-11-14 11:34:06 -0500

    add api and add new js file

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,46 @@
 
-// var express = require('express');
-// var router = express.Router();
-
-// var searchTerms = "";
-// var api = "http://api.walmartlabs.com/v1/search?query=" + searchTerms + "&format=json&apiKey=8dq9cz3zggzevwma8bc842za";
-
-// router.get('/api' , function(req, res){
-//   // var searchTerms = "hat";
-//   console.log('hello');
-
-
-// });
-
-
-
-
-
+var http = require('http');
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+
+
+var apiKey = '8dq9cz3zggzevwma8bc842za';
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  	res.send('api');
+  	var url = "http://api.walmartlabs.com/v1/search?query=bed&format=json&apiKey=8dq9cz3zggzevwma8bc842za";
+  	var options = {
+	  host: "api.walmartlabs.com",
+	  port: 80,
+	  path: '/v1/search?query=bed&format=json&apiKey=8dq9cz3zggzevwma8bc842za',
+	  method: 'GET',
+	  json: true
+	};
+
+	
+	var temp = "";
+  	
+
+	http.request(options, function(response) {
+	  console.log('STATUS: ' + response.statusCode);
+	  console.log('HEADERS: ' + JSON.stringify(res.headers));
+	  response.setEncoding('utf8');
+	  response.on('data', function (data) {
+	    //console.log('BODY: ' + data);
+	    //console.log('BODY: ' + data);
+	     temp += data;
+	    
+	  });
+	  response.on('end', function () {
+    	res.writeHead(200, {"Content-Type": "application/json"});
+		res.end(temp);
+    	console.log(temp);
+  	  });
+
+	});
+	
 });
 
 module.exports = router;
